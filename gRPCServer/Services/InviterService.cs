fa26e60983df64bc443b3435f8efcf302f16ee52
Rustem Shaiheraziev <mr.shaykheraziev@mail.ru>
2023-10-05 08:25:07 +0300

    Добавьте файлы проекта.

---
+++ gRPCServer/Services/InviterService.cs
@@ -0,0 +1,23 @@
+﻿using Google.Protobuf.WellKnownTypes; // пространство имен для Timestamp и Duration
+using Grpc.Core;
+
+namespace gRPCServer.Services
+{
+    public class InviterService: Inviter.InviterBase
+    {
+        public override Task<Response> Invite(Request request, ServerCallContext context)
+        {
+            var eventDateTime = DateTime.UtcNow.AddDays(1);
+
+            var duration = TimeSpan.FromHours(2);
+
+            return Task.FromResult(new Response
+            {
+                Invitation = $"{request.Name}, приглашаем вас посетить мероприятие",
+                Start = Timestamp.FromDateTime(eventDateTime),
+                Duration = Duration.FromTimeSpan(duration)
+            });
+        }
+    }
+    
+}
---
+++ gRPCServer/Services/MessengerService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Stream;
+
+namespace gRPCServer.Services
+{
+    public class MessengerService: Messenger.MessengerBase
+    {
+        string[] messages = { "Привет", "Норм", "...", "Нет", "пока" };
+        public override async Task DataStream(IAsyncStreamReader<Stream.Request> requestStream, IServerStreamWriter<Responce> responseStream, ServerCallContext context)
+        {
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var message in requestStream.ReadAllAsync())
+                {
+                    Console.WriteLine($"Client: {message.Content}");
+                }
+            });
+
+            foreach (var message in messages)
+            {
+                if (!readTask.IsCompleted)
+                {
+                    await responseStream.WriteAsync(new Responce { Content = message });
+                    Console.WriteLine(message);
+                    await Task.Delay(2000);
+                }
+            }
+        }
+    }
+}
